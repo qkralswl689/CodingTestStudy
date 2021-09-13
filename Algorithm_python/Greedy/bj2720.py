f505c80f452232bf5152e9292ea4847d24108e53
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-09-13 20:41:30 +0900

    Update bj2720.py

--- Algorithm_python/Greedy/bj2720.py
+++ Algorithm_python/Greedy/bj2720.py
@@ -1,14 +1,16 @@
 import sys
 
-coin = [25, 10, 5, 1]
-n = int(sys.stdin.readline())
-n_list = []
-for i in range(n):
-    n_list.append(int(sys.stdin.readline()))
+coin = [25,10,5,1]
 
-for i in n_list :
+t = int(sys.stdin.readline())
+t_list = []
+for i in range(t) :
+    t_list.append(int(sys.stdin.readline()))
+
+for i in t_list :
     c = []
-    for j in range(4) :
+    for j in range(len(coin)) :
+        # // : 나누기 연산 후 소수점 이하의 수를 버리고, 정수 부분의 수만 구함
         c.append(i // coin[j])
         i = i % coin[j]
 
